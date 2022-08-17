0bebb129f8e3584dba0ef077e1274be2c910aa6c
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-08-17 16:54:01 +0800

    this took me more than it should've

--- src/thing/ThisThing.java
+++ src/thing/ThisThing.java
@@ -1,10 +1,37 @@
 package thing;
 
+import arc.*;
+import arc.files.*;
+import arc.struct.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
 import mindustry.mod.*;
 import thing.content.*;
 import thing.logic.*;
 
+import java.lang.reflect.*;
+
+import static mindustry.Vars.*;
+
 public class ThisThing extends Mod{
+    public ThisThing(){
+        Events.on(ClientLoadEvent.class, e -> {
+            Field f = Sounds.class.getDeclaredField("idToSound");
+            f.setAccessible(true);
+            IntMap idToSound = f.get(Sounds.class);
+            for(int i = 0; i < idToSound.size; i++){
+                // swish can breaks game audio sooooooo
+                if(i == Sounds.getSoundId(Sounds.swish)) return;
+                Sound sound = Sounds.getSound(i);
+                Field f2 = sound.class.getDeclaredField("file");
+                f2.setAccessible(true);
+                Fi file = f2.get(sound);
+                logicVars.put("@sfx" + file.nameWithoutExtension(), sound);
+                logicVars.put("@sfx" + i, sound);
+            }
+        });
+    }
+    
     @Override
     public void loadContent(){
         TTBlocks.load();
