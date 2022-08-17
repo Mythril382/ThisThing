745135ebce120ac805a1bf7a45374cfff0ae3367
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-08-17 13:15:05 +0800

    let's see....

--- src/thing/ThisThing.java
+++ src/thing/ThisThing.java
@@ -1,5 +1,7 @@
 package thing;
 
+import arc.func.*;
+import mindustry.logic.*;
 import mindustry.mod.*;
 import thing.content.*;
 
@@ -7,5 +9,15 @@ public class ThisThing extends Mod{
     @Override
     public void loadContent(){
         TTBlocks.load();
+    }
+    
+    /** Mimics the RegisterStatement annotation. Credits to Deltanedas for creating the original function.
+     * @param name The name of the statement to be registered.
+     * @param func The Func to be added to the custom parsers list.
+     * @param prov The Prov to be added to the all statements list.
+    */
+    public static void registerStatement(String name, Func<String[], LStatement> func, Prov<LStatement> prov){
+        LAssembler.customParsers.put(name, func);
+        LogicIO.allStatements.add(prov);
     }
 }
