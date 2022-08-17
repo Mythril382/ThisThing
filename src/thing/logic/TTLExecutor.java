1512086e4c0c7d92d784c76a964011c749a450cf
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-08-17 13:38:34 +0800

    intensity

--- src/thing/logic/TTLExecutor.java
+++ src/thing/logic/TTLExecutor.java
@@ -7,17 +7,17 @@ import mindustry.logic.LExecutor.*;
 
 public class TTLExecutor{
     public static class ShakeI implements LInstruction{
-        public int duration, x, y;
+        public int intensity, x, y;
         
         public ShakeI(int duration, int x, int y){
-            this.duration = duration;
+            this.intensity = intensity;
             this.x = x;
             this.y = y;
         }
         
         @Override
         public void run(LExecutor exec){
-            Effect.shake(exec.numf(duration), exec.numf(duration), World.unconv(exec.numf(x)), World.unconv(exec.numf(y)));
+            Effect.shake(exec.numf(intensity), exec.numf(intensity), World.unconv(exec.numf(x)), World.unconv(exec.numf(y)));
         }
     }
 }
