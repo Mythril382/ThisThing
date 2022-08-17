3307985c9e039bf9ada9c30e3235bde123e95ca0
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-08-17 17:05:56 +0800

    try catch

--- src/thing/ThisThing.java
+++ src/thing/ThisThing.java
@@ -17,19 +17,23 @@ import static mindustry.Vars.*;
 public class ThisThing extends Mod{
     public ThisThing(){
         Events.on(ClientLoadEvent.class, e -> {
-            Field f = Sounds.class.getDeclaredField("idToSound");
-            f.setAccessible(true);
-            IntMap idToSound = (IntMap)f.get(Sounds.class);
-            for(int i = 0; i < idToSound.size; i++){
-                // swish can breaks game audio sooooooo
-                if(i == Sounds.getSoundId(Sounds.swish)) return;
-                Sound sound = Sounds.getSound(i);
-                Field f2 = sound.getClass().getDeclaredField("file");
-                f2.setAccessible(true);
-                Fi file = (Fi)f2.get(sound);
-                logicVars.put("@sfx" + file.nameWithoutExtension(), sound);
-                logicVars.put("@sfx" + i, sound);
-            }
+            try{
+                Field f = Sounds.class.getDeclaredField("idToSound");
+                f.setAccessible(true);
+                IntMap idToSound = (IntMap)f.get(Sounds.class);
+                for(int i = 0; i < idToSound.size; i++){
+                    // swish can breaks game audio sooooooo
+                    if(i == Sounds.getSoundId(Sounds.swish)) return;
+                    Sound sound = Sounds.getSound(i);
+                    try{
+                        Field f2 = sound.getClass().getDeclaredField("file");
+                        f2.setAccessible(true);
+                        Fi file = (Fi)f2.get(sound);
+                        logicVars.put("@sfx" + file.nameWithoutExtension(), sound);
+                        logicVars.put("@sfx" + i, sound);
+                    }catch(Throwable ignoreAgain){}
+                }
+            }catch(Throwable ignore){}
         });
     }
     
