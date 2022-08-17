86b06290bd96a60232eb61b2cb476f0f88568926
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-08-17 13:51:11 +0800

    that clearly doesn't work

--- src/thing/logic/TTLStatements.java
+++ src/thing/logic/TTLStatements.java
@@ -35,5 +35,5 @@ public class TTLStatements{
             return LCategory.world;
         }
     }
-    ThisThing.registerStatement("shake", strArr -> { return new ShakeStatement(); }, () -> { return new ShakeStatement(); });
+    ThisThing.registerStatement("shake", (String[] strArr) -> new ShakeStatement(), () -> new ShakeStatement());
 }
