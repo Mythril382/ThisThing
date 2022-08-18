4fc4dfa40684068137de121351696545d4bfe857
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-08-18 13:51:51 +0800

    why can't i do that

--- src/thing/ThisThing.java
+++ src/thing/ThisThing.java
@@ -44,7 +44,7 @@ public class ThisThing extends Mod{
             Time.runTask(6f, () -> {
                 if(mobile) mobileButton();
                 else desktopButton();
-                Events.on(ResizeEvent.class, () -> {
+                Events.on(ResizeEvent.class, ev -> {
                     if(mobile) mobileButton();
                     else desktopButton();
                 });
