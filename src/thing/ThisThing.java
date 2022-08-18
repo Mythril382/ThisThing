6a724e5cce2dc7c07151276dd3f259820272dfc5
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-08-18 16:20:36 +0800

    logic statements in database (real)

--- src/thing/ThisThing.java
+++ src/thing/ThisThing.java
@@ -3,18 +3,20 @@ package thing;
 import arc.*;
 import arc.audio.*;
 import arc.files.*;
-import arc.scene.style.*;
+import arc.math.*;
+import arc.scene.ui.*;
 import arc.scene.ui.layout.*;
 import arc.struct.*;
 import arc.util.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.logic.LStatements.*;
 import mindustry.mod.*;
 import mindustry.ui.*;
+import mindustry.ui.dialogs.*;
 import thing.content.*;
 import thing.logic.*;
-
-import java.lang.reflect.*;
 
 import static arc.Core.*;
 import static mindustry.Vars.*;
@@ -22,31 +24,42 @@ import static mindustry.Vars.*;
 public class ThisThing extends Mod{
     public ThisThing(){
         Events.on(ClientLoadEvent.class, e -> {
-            try{
-                Field f = Sounds.class.getDeclaredField("idToSound");
-                f.setAccessible(true);
-                IntMap idToSound = (IntMap)f.get(Sounds.class);
-                for(int i = 0; i < idToSound.size; i++){
-                    // swish can break game audio sooooooo
-                    if(i != Sounds.getSoundId(Sounds.swish)){
-                        Sound sound = Sounds.getSound(i);
-                        try{
-                            Field f2 = sound.getClass().getDeclaredField("file");
-                            f2.setAccessible(true);
-                            Fi file = (Fi)f2.get(sound);
-                            String name = file.nameWithoutExtension();
-                            logicVars.put("@sfx" + name, sound);
-                        }catch(Throwable ignoreAgain){}
-                    }
+            IntMap idToSound = (IntMap)Reflect.get(Sounds.class, "idToSound");
+            for(int i = 0; i < idToSound.size; i++){
+                // swish can break game audio sooooooo
+                if(i != Sounds.getSoundId(Sounds.swish)){
+                    Sound sound = Sounds.getSound(i);
+                    Fi file = (Fi)Reflect.get(sound, "file");
+                    String name = file.nameWithoutExtension();
+                    logicVars.put("@sfx" + name, sound);
                 }
-            }catch(Throwable ignore){}
+            }
             
             Time.runTask(6f, () -> {
-                if(mobile) mobileButton();
-                else desktopButton();
-                Events.on(ResizeEvent.class, ev -> {
-                    if(mobile) mobileButton();
-                    else desktopButton();
+                DatabaseDialog db = ui.database;
+                Table all = (Table)Reflect.get(db, "all");
+                db.resizedShown(() -> {
+                    all.add("@content.lst.name").growX().left().color(Pal.accent);
+                    all.row();
+                    all.image().growX().pad(5).padLeft(0).padRight(0).height(3).color(Pal.accent);
+                    all.row();
+                    all.table(list -> {
+                        list.left();
+                        int cols = (int)Mathf.clamp((graphics.getWidth() - Scl.scl(30)) / Scl.scl(32 + 12), 1, 22);
+                        int count = 0;
+                        for(int i = 0; i < LogicIO.allStatements.size; i++){
+                            LStatement lst = LogicIO.allStatements.get(i).get();
+                            if(!(lst instanceof InvalidStatement)){
+                                Image image = new Image(atlas.find("this-thing-lst-icon"));
+                                list.add(image).size(8 * 4).pad(3);
+                                image.clicked(() -> {
+                                    ui.showInfo("[accent]Name[]: " + lst.name() + "\n[accent]Category[]: " + "[#" lst.category().color "]" + lst.category().localized() + "[]");
+                                });
+                                image.addListener(new Tooltip(t -> t.setBackground(Tex.button).add("[#" + lst.category().color + "]" + lst.name())));
+                            }
+                        }
+                    });
+                    all.row();
                 });
             });
         });
@@ -56,18 +69,5 @@ public class ThisThing extends Mod{
     public void loadContent(){
         TTBlocks.load();
         TTLStatements.load();
-    }
-    
-    public static void mobileButton(){
-        Table buttons = ui.menuGroup.<Table>find("buttons");
-        
-        // Credits to Liplum (Creator of CyberIO) for this piece of code.
-        boolean row = buttons.getChildren().count(b -> b instanceof MobileButton) % (graphics.isPortrait() ? 2 : 4) == 0;
-        if(row) buttons.row();
-        
-        buttons.add(new MobileButton(new TextureRegionDrawable(atlas.find("this-thing-button")), "@this-thing-button", () -> {})).name("@this-thing-button");
-    }
-    
-    public static void desktopButton(){
     }
 }
