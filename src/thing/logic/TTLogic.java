bb7ed4265141361f01ac3d7bd0eafea4e1f7eed5
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-08-22 02:27:36 +0800

    gam
    ing

--- src/thing/logic/TTLogic.java
+++ src/thing/logic/TTLogic.java
@@ -11,6 +11,9 @@ import mindustry.logic.*;
 import static mindustry.Vars.*;
 
 public class TTLogic{
+    public static final LCategory
+    categoryMisc = new LCategory("misc", Pal.heal.cpy().mul(0.8f), Icon.iconSmall);
+    
     public static void load(){
         TTLStatements.load();
         
