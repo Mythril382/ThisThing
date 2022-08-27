3f51372c113fd1336f54c207f9869af12b8119ca
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-08-27 14:02:26 +0800

    yes

--- src/thing/ThisThing.java
+++ src/thing/ThisThing.java
@@ -1,13 +1,24 @@
 package thing;
 
+import arc.struct.*;
+import mindustry.ai.*;
 import mindustry.mod.*;
+import thing.ai.*;
 import thing.content.*;
 import thing.logic.*;
+
+import static mindustry.Vars.*;
 
 public class ThisThing extends Mod{
     @Override
     public void loadContent(){
         TTBlocks.load();
         TTLogic.load();
+        
+        content.units().each(unit -> {
+            Seq<UnitCommand> cmds = Seq.with(unit.commands);
+            cmds.add(UCommands.patrolCommand);
+            unit.commands = cmds.toArray(UnitCommand.class);
+        });
     }
 }
