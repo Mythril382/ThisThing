97d98c8fa2e8914780d9aac796bf8ad7ae0378e8
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-08-30 22:01:34 +0800

    among

---
+++ src/thing/logic/LStringMethods.java
@@ -0,0 +1,12 @@
+package thing.logic;
+
+public enum LStringMethods{
+    charAt,
+    concat,
+    isEmpty,
+    length,
+    toLowerCase,
+    toUpperCase;
+    
+    public static final LStringMethods[] all = values();
+}
