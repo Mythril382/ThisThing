614a330ab0b09c1076d4d98e673417a71d3373ef
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-09-01 22:13:11 +0800

    arrival gif code

--- src/thing/ThisThing.java
+++ src/thing/ThisThing.java
@@ -1,14 +1,34 @@
 package thing;
 
-import arc.struct.*;
-import mindustry.ai.*;
+import arc.*;
+import arc.graphics.g2d.*;
+// import arc.struct.*;
+import arc.util.*;
+// import mindustry.ai.*;
+import mindustry.game.EventType.*;
 import mindustry.mod.*;
-import thing.ai.*;
+// import thing.ai.*;
 import thing.logic.*;
 
 import static mindustry.Vars.*;
 
 public class ThisThing extends Mod{
+    public static TextureRegion arrival;
+    
+    public ThisThing(){
+        Events.on(ClientLoadEvent.class, e -> {
+            TextureRegion[] arrivals = new TextureRegion[8];
+            for(int i = 0; i <= 7; i++){
+                arrivals[i] = Core.atlas.find("this-thing-arrival" + i);
+            }
+            arrival = arrivals[0];
+            Events.run(Trigger.update, () -> {
+                int arrivalFrame = (int)((Time.globalTime / 5f) % arrivals.length);
+                arrival.set(arrivals[arrivalFrame]);
+            });
+        });
+    }
+    
     @Override
     public void loadContent(){
         TTLogic.load();
