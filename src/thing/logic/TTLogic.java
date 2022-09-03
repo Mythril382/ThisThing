d391a701ea534de7f647c71807a2fd8c260946d0
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-09-04 00:32:08 +0800

    pain

--- src/thing/logic/TTLogic.java
+++ src/thing/logic/TTLogic.java
@@ -17,15 +17,14 @@ public class TTLogic{
     public static void load(){
         TTLStatements.load();
         
-        IntMap idToSound = (IntMap)Reflect.get(Sounds.class, "idToSound");
+        /* IntMap idToSound = (IntMap)Reflect.get(Sounds.class, "idToSound");
         for(int i = 0; i < idToSound.size; i++){
-            // swish can break game audio sooooooo
             if(i != Sounds.getSoundId(Sounds.swish)){
                 Sound sound = Sounds.getSound(i);
                 Fi file = (Fi)Reflect.get(sound, "file");
                 String name = file.nameWithoutExtension();
                 logicVars.put("@sfx" + name, sound);
             }
-        }
+        } */
     }
 }
