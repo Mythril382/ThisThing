48ed4bcb4c54f2dec4caf86ddb217e30fc2ddbbf
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-09-08 19:41:35 +0800

    ok pal

--- src/thing/logic/TTLogic.java
+++ src/thing/logic/TTLogic.java
@@ -19,7 +19,10 @@ public class TTLogic{
     
     public static void load(){
         TTLStatements.load();
-        
+        setupConstants();
+    }
+    
+    public static void setupConstants(){
         /* IntMap idToSound = (IntMap)Reflect.get(Sounds.class, "idToSound");
         for(int i = 0; i < idToSound.size; i++){
             if(i != Sounds.getSoundId(Sounds.swish)){
@@ -39,6 +42,18 @@ public class TTLogic{
                 try{
                     Color col = (Color)f.get(Color.class);
                     logicVars.put("@col" + f.getName(), Color.toDoubleBits(col.r, col.g, col.b, col.a));
+                }catch(Throwable ignored2){}
+            });
+        }catch(Throwable ignored){}
+        
+        try{
+            Field[] fieldsArr = Pal.class.getFields();
+            Seq<Field> fields = new Seq<>(fieldsArr);
+            fields = fields.select(f -> Color.class.isAssignableFrom(f.getType()));
+            fields.each(f -> {
+                try{
+                    Color col = (Color)f.get(Pal.class);
+                    logicVars.put("@pal" + f.getName(), Color.toDoubleBits(col.r, col.g, col.b, col.a));
                 }catch(Throwable ignored2){}
             });
         }catch(Throwable ignored){}
