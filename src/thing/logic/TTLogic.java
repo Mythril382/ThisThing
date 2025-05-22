8af0eca15bf0298f3454302b8a8957e1c6d620bd
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2025-05-22 11:47:03 +0800

    playsound is real

--- src/thing/logic/TTLogic.java
+++ src/thing/logic/TTLogic.java
@@ -1,14 +1,8 @@
 package thing.logic;
 
-import arc.audio.*;
-import arc.files.*;
-import arc.struct.*;
-import arc.util.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
 import mindustry.logic.*;
-
-import java.lang.reflect.*;
 
 import static mindustry.Vars.*;
 
@@ -18,18 +12,5 @@ public class TTLogic{
     
     public static void load(){
         TTLStatements.load();
-        setupConstants();
-    }
-    
-    public static void setupConstants(){
-        IntMap idToSound = (IntMap)Reflect.get(Sounds.class, "idToSound");
-        for(int i = 0; i < idToSound.size; i++){
-            if(i != Sounds.getSoundId(Sounds.swish)){
-                Sound sound = Sounds.getSound(i);
-                Fi file = (Fi)Reflect.get(sound, "file");
-                String name = file.nameWithoutExtension();
-                logicVars.put("$" + name, i);
-            }
-        }
     }
 }
