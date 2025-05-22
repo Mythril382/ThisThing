a020609e7e428e1568b987cf94f46cc3cf3888f8
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2025-05-22 11:47:40 +0800

    a

--- src/thing/logic/TTLogic.java
+++ src/thing/logic/TTLogic.java
@@ -4,8 +4,6 @@ import mindustry.gen.*;
 import mindustry.graphics.*;
 import mindustry.logic.*;
 
-import static mindustry.Vars.*;
-
 public class TTLogic{
     public static final LCategory
     categoryMisc = new LCategory("misc", Pal.heal.cpy().mul(0.75f), Icon.effectSmall);
